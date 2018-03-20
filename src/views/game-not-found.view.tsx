6491338919083eaae386b0edfd963064a60f8215
kingdaro <kingdaro@gmail.com>
2018-03-20 11:01:34 -0400

    show new game link on not found page

--- src/views/game-not-found.view.tsx
+++ src/views/game-not-found.view.tsx
@@ -3,5 +3,12 @@ import { GameID } from "../models/game.model"
 import { Layout } from "./layout.view"
 
 export function GameNotFoundPage({ id }: { id: GameID }) {
-  return <Layout>couldn't find game {id}</Layout>
+  return (
+    <Layout>
+      <p>couldn't find game #{id}</p>
+      <p>
+        <a href="/games/create">make a new one?</a>
+      </p>
+    </Layout>
+  )
 }
